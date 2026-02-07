1452d745aa467f680408d2ff8d4153c5c45f8242
Caique  Augusto <caiquekola@hotmail.com>
2026-02-06 21:48:05 -0300

    comeco

---
+++ HtmlAnalyzer.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.net.Authenticator;
+import java.net.InetSocketAddress;
+import java.net.ProxySelector;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpClient.Redirect;
+import java.net.http.HttpClient.Version;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.time.Duration;
+
+public class HtmlAnalyzer {
+    public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
+        System.out.println("Hello world, i'm back!");
+        System.out.println(Duration.ofSeconds(10));
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(new URI("http://hiring.axreng.com/internship/example1.html"))
+            .timeout(Duration.ofSeconds(10))
+            .GET()
+            .build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        System.out.println(response.body());
+        String[] lines = response.body().split("\\R");
+        int idxI = response.body().indexOf("<body>", 0)+6;
+        int idxE = response.body().indexOf("</body>",idxI);
+        System.out.println("idxI "+idxI + " - idxE "+idxE);
+        System.out.println(response.body().substring(idxI,idxE).trim());
+        for (int i = 0; i < lines.length; i++) {
+            
+                System.out.println("Linha "+i+":"+lines[i]);
+        }
+    }
+}
